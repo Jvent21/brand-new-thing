9c1887f17626218b766705d022e75aa276e5c6af
Jason Ventura <jasonv@email.uscupstate.edu>
2017-04-12 22:36:54 +0000

    Functional sign up

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   
   get '/about', to: 'static_pages#about'
 
+  get '/signup', to: 'users#new'
+  
+  post '/signup', to: 'users#create'
+  
+  resources :users
+
   root "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
